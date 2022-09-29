c6a538a9a069bc3673955e37811c08488196027c
FireflyStars <gotodev7@gmail.com>
2022-09-29 16:08:39 +0800

    Finished import detailing item from infoitems

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -43,74 +43,98 @@ class ImportController extends Controller
     }
 
     public function importDetailingItem(){
-        ini_set('max_execution_time', 6000);
-        $typeItems = DB::table('typeitem')->select('name')->get()->map(function($value,$key){
-            return collect($value)->values();
-          })->collapse()->toArray();
-        $newTypeItems = DB::table('infoitems')->whereNotIn('typeitem', $typeItems)->select('typeitem')->get()->unique();
-        foreach ($newTypeItems as $key => $item) {
-            DB::table('typeitemmatrix')->insert([
-                'oldword'=> '',
-                'newword'=> $item->typeitem,
-            ]);
-        }
-        dd("done");
-        return;
-        $infoItems = DB::table('infoitems')->take(10)->get();
-        foreach ($infoItems as $item) {
-            if(DB::table('detailingitem_test')->where('tracking', $item->ItemTrackingKey)->count() == 0){
-                $customerUUID = DB::table('infoInvoice')->where('InvoiceID', $item->InvoiceID)->value('CustomerID');
-                $customerId = DB::table('infoCustomer')->where('CustomerID', $customerUUID)->value('id');
-                $departmentId = DB::table('departments')->where('name', $item->DepartmentName)->value('id');
-                $typeItem = DB::table('typeitem')->where('name', $item->typeitem)->select('id', 'category_id')->first();
-                $brandId = DB::table('brands')->where('name', $item->brand)->value('id');
-                $fabricIds = DB::table('fabrics')->where('name', $item->Fabrics)->select('id')->get()->map(function($value,$key){
-                    return collect($value)->values();
-                  })->collapse()->toJson();
-                $colorIds = DB::table('colours')->where('name', $item->Colors)->select('id')->get()->map(function($value,$key){
-                    return collect($value)->values();
-                  })->collapse()->toJson();
-                $complexitiesIds = DB::table('complexities')->where('name', $item->Complexities)->select('id')->get()->map(function($value,$key){
-                    return collect($value)->values();
-                  })->collapse()->toJson();
-                $patternID = DB::table('patterns')->where('name', $item->Patterns)->value('id');
-                $conditionID = DB::table('conditions')->where('name', $item->generalState)->value('id');
-                $detailItemData = [
-                    'item_id'   => 0,
-                    'status'    => 'olditem',
-                    'tracking'  => $item->ItemTrackingKey,
-                    'order_id'  => 0,
-                    'InvoiceID'  => $item->InvoiceID,
-                    'etape'  => 11,
-                    'created_at'  => now(),
-                    'updated_at'  => now(),
-                    'pricecleaning'  => 0,
-                    'coeftailoringbrand'  => 0,
-                    'coefcleaningbrand'  => 0,
-                    'coeftailoringfabric'  => 0,
-                    'coefcleaningfabric'  => 0,
-                    'coefcleaningcomplexities'  => 0,
-                    'coeftailoringcomplexities'  => 0,
-                    'cleaning_services'  => json_encode([1, 3]),
-                    'tailoring_price_type'  => 'Standard',
-                    'cleaning_price_type'  => 'Standard',
-                    'tailoring_services'  => json_encode([]),
-                    'cleaning_addon_price'  => 0,
-                    'tailoring_price'  => 0,
-                    'customer_id'  => $customerId,
-                    'department_id'  => $departmentId,
-                    'typeitem_id'  => $typeItem ? $typeItem->id : null,
-                    'category_id'  => $typeItem ? $typeItem->category_id : null,
-                    'brand_id'  => $brandId,
-                    'fabric_id'  => $fabricIds,
-                    'color_id'  => $colorIds,
-                    'complexities_id'  => $complexitiesIds,
-                    'pattern_id'  => $patternID,
-                    'condition_id'  => $conditionID,
-                ];
-                DB::table('detailingitem_test')->insert($detailItemData);
+        ini_set('max_execution_time', 60000);
+        // $brands = DB::table('brands')->select('name')->get()->map(function($value,$key){
+        //     return collect($value)->values();
+        //   })->collapse();
+        // $neweBrands = DB::table('infoitems')->whereNotIn('brand', $brands)->select(DB::raw('COUNT(*) as cnt'), 'brand')->groupBy('brand')->get();
+        // foreach ($neweBrands as $brand) {
+        //     DB::table('brandmatrix')->insert([
+        //         'oldword'=> '',
+        //         'newword'=> $brand->brand,
+        //         'nvofline'=> $brand->cnt,
+        //     ]);
+        // }
+        // dd("done");
+        DB::table('infoitems')->orderBy('id')->chunk(1000, function($infoItems){
+            foreach ($infoItems as $item) {
+                if(DB::table('detailingitem_copy')->where('tracking', $item->ItemTrackingKey)->count() == 0){
+                    $typeItem = DB::table('typeitem')->where('name', $item->typeitem)->select('id', 'category_id', 'department_id')->first();
+                    if($typeItem == null){
+                        $oldName = DB::table('typeitemmatrix')->where('newword', $item->typeitem)->value('oldword');
+                        if($oldName != ''){
+                            $typeItem = DB::table('typeitem')->where('name', $oldName)->select('id', 'category_id', 'department_id')->first();
+                        }else{
+                            continue;
+                        }
+                    }
+                    
+                    if($item->brand == '' || $item->brand = 'a Brand Not Listed' || $item->brand = 'aaa unbranded' || $item->brand = 'Xxx Unbranded Xxx'){
+                        $brandId = 3;
+                    }else{
+                        $brand = DB::table('brands')->where('name', $item->brand)->first();
+                        if($brand == null){
+                            $oldName = DB::table('brandmatrix')->where('newword', $item->brand)->value('oldword');
+                            if($oldName != ''){
+                                $brandId = DB::table('brands')->where('name', $oldName)->value('id');
+                            }else{
+                                $brandId = 3;
+                            }
+                        }
+                    }
+                    $customerUUID = DB::table('infoInvoice')->where('InvoiceID', $item->InvoiceID)->value('CustomerID');
+                    $customerId = DB::table('infoCustomer')->where('CustomerID', $customerUUID)->value('id');
+                    if($typeItem && $brandId && $customerId){
+                        $fabricIds = DB::table('fabrics')->where('name', $item->Fabrics)->select('id')->get()->map(function($value,$key){
+                            return collect($value)->values();
+                            })->collapse()->toJson();
+                        $colors = explode(", ", $item->Colors);
+                        $colorIds = DB::table('colours')->whereIn('name', $colors)->select('id')->get()->map(function($value,$key){
+                            return collect($value)->values();
+                            })->collapse()->toJson();
+                        $complexitiesIds = DB::table('complexities')->where('name', $item->Complexities)->select('id')->get()->map(function($value,$key){
+                            return collect($value)->values();
+                            })->collapse()->toJson();
+                        $patternID = DB::table('patterns')->where('name', $item->Patterns)->value('id');
+                        $conditionID = DB::table('conditions')->where('name', $item->generalState)->value('id');
+                        $detailItemData = [
+                            'item_id'   => $item->id,
+                            'status'    => 'Completed',
+                            'tracking'  => $item->ItemTrackingKey,
+                            'order_id'  => 0,
+                            'InvoiceID'  => $item->InvoiceID,
+                            'etape'  => 11,
+                            'created_at'  => now(),
+                            'updated_at'  => now(),
+                            'pricecleaning'  => 0,
+                            'coeftailoringbrand'  => 0,
+                            'coefcleaningbrand'  => 0,
+                            'coeftailoringfabric'  => 0,
+                            'coefcleaningfabric'  => 0,
+                            'coefcleaningcomplexities'  => 0,
+                            'coeftailoringcomplexities'  => 0,
+                            'cleaning_services'  => json_encode([1, 3]),
+                            'tailoring_price_type'  => 'Standard',
+                            'cleaning_price_type'  => 'Standard',
+                            'tailoring_services'  => json_encode([]),
+                            'cleaning_addon_price'  => 0,
+                            'tailoring_price'  => 0,
+                            'customer_id'  => $customerId,
+                            'department_id'  => $typeItem->department_id,
+                            'typeitem_id'  => $typeItem->id,
+                            'category_id'  => $typeItem->category_id,
+                            'brand_id'  => $brandId,
+                            'fabric_id'  => $fabricIds,
+                            'color_id'  => $colorIds,
+                            'complexities_id'  => $complexitiesIds,
+                            'pattern_id'  => $patternID,
+                            'condition_id'  => $conditionID,
+                        ];
+                        DB::table('detailingitem_copy')->insert($detailItemData);
+                    }
+                }
             }
-        }
+        });
         dd("done");
     }
 }
